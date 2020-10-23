99ac7747318d28f9ef5fea340252c08ab9aa4849
AliNajafi <AliNajafiTabriz@gmail.com>
2020-10-24 01:52:22 +0330

    Initiate index page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let searchInputPlaceholder = document.getElementById("search-input-placeholder");
+let searchInput = document.getElementById("search-input");
+searchInput.addEventListener("click", () => {
+    searchInputPlaceholder.style.visibility = "hidden";
+})
+searchInput.addEventListener("focusout", () => {
+    if (searchInput.value === "") {
+        searchInputPlaceholder.style.visibility = "visible";
+    }
+})
+searchInputPlaceholder.addEventListener("click", () => {
+    searchInputPlaceholder.style.visibility = "hidden";
+    searchInput.focus();
+})
+searchInputPlaceholder.addEventListener("focusout", () => {
+    if (searchInput.value === "") {
+        searchInputPlaceholder.style.visibility = "visible";
+    }
+})
+searchInput.addEventListener("input", () => {
+    if (searchInput.value === "") {
+        searchInputPlaceholder.style.visibility = "visible";
+    }
+})
