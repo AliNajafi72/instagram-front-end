c91df15d28a1ba113cb8fa639f5f58fc6eafe852
AliNajafi <AliNajafiTabriz@gmail.com>
2020-10-25 03:05:48 +0330

    Add saved stories to index page

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-let searchInputPlaceholder = document.getElementById("search-input-placeholder");
-let searchInput = document.getElementById("search-input");
-searchInput.addEventListener("click", () => {
-    searchInputPlaceholder.style.visibility = "hidden";
-})
-searchInput.addEventListener("focusout", () => {
-    if (searchInput.value === "") {
-        searchInputPlaceholder.style.visibility = "visible";
-    }
-})
-searchInputPlaceholder.addEventListener("click", () => {
-    searchInputPlaceholder.style.visibility = "hidden";
-    searchInput.focus();
-})
-searchInputPlaceholder.addEventListener("focusout", () => {
-    if (searchInput.value === "") {
-        searchInputPlaceholder.style.visibility = "visible";
-    }
-})
-searchInput.addEventListener("input", () => {
-    if (searchInput.value === "") {
-        searchInputPlaceholder.style.visibility = "visible";
-    }
-})
+$(document).ready(function(){
+    let searchInputPlaceholder = document.getElementById("search-input-placeholder");
+    let searchInput = document.getElementById("search-input");
+    searchInput.addEventListener("click", () => {
+        searchInputPlaceholder.style.visibility = "hidden";
+    })
+    searchInput.addEventListener("focusout", () => {
+        if (searchInput.value === "") {
+            searchInputPlaceholder.style.visibility = "visible";
+        }
+    })
+    searchInputPlaceholder.addEventListener("click", () => {
+        searchInputPlaceholder.style.visibility = "hidden";
+        searchInput.focus();
+    })
+    searchInputPlaceholder.addEventListener("focusout", () => {
+        if (searchInput.value === "") {
+            searchInputPlaceholder.style.visibility = "visible";
+        }
+    })
+    searchInput.addEventListener("input", () => {
+        if (searchInput.value === "") {
+            searchInputPlaceholder.style.visibility = "visible";
+        }
+    })
+    $('.saved-stories-inner').slick({
+        slidesToShow: 8,
+        prevArrow: false,
+        nextArrow: false
+    })
+});
